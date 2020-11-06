8b2f3ee420f499750087b4cdc3c3ebb1522b386a
Hanhan Chen <chenhanhan1989@gmail.com>
2020-11-06 22:58:54 +0100

    los string son un arrray de caracteres

---
+++ strings_array.js
@@ -0,0 +1,13 @@
+// Los strings son la suma de carácteres
+
+var nombre = "hanhan";
+for(var i = 0; i < nombre.length; i++){
+  console.log(nombre[i]);
+}
+
+// crear nuevo array de caractéres
+
+var nombre = "hanhan";
+var caracteresDelNombre = Array.from(nombre);
+console.log(caracteresDelNombre);
+
